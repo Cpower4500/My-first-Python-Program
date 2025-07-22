1c5f74d4c76db664ec3bc5318a2e580f412fc9e0
Cpower4500 <powerchekesha48@gmail.com>
2025-07-21 21:44:01 -0400

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,29 @@
+Your main branch isn't protected
+Protect this branch from force pushing or deletion, or require status checks before merging. View documentation.
+Go to file
+t
+Name		
+Cpower4500
+Cpower4500
+Add files via upload
+c5e8132
+ · 
+2 months ago
+DEBUG07-01_Chekesha.txt
+Add files via upload
+2 months ago
+DEBUG07-02_Chekesha.txt
+Add files via upload
+2 months ago
+Module_8_Exercise_1_Merge_Pseudocode_Chekesha.txt
+Add files via upload
+2 months ago
+My first Python Progam
+(raining_cats_and_dogs.py).
+3 months ago
+README.md
+Update README.md
+3 months ago
+Repository files navigation
+README
+My-first-Python-Program
